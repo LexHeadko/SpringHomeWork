f0c730cf98bfac9c2b11e4807980895ff8186daf
LexHeadko <n-odes11rus@yandex.ru>
2023-07-30 16:49:50 +0200

    initial commit

---
+++ src/main/java/com/example/springhomework/SpringHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.springhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHomeWorkApplication.class, args);
+    }
+
+}
